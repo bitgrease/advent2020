a73df1596449a93e09159a1022113127036380b8
Kelly Netterville <bitgrease@gmail.com>
2020-12-01 18:58:55 -0500

    first commit

---
+++ day1_expense_report.py
@@ -0,0 +1,7 @@
+# Approach: Starting from top of list, subtract value from 2020
+# Search remainder of list for result.
+# If found, get product and finish, if not, go to next entry in list.
+
+# read input file into tuple
+# loop through list using logic above
+
