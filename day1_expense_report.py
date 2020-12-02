a4ceb6530069849b8ab1263bc664cf1509553af0
Kelly Netterville <bitgrease@gmail.com>
2020-12-02 11:35:48 -0500

    Solution to D1 Part 1. Started file for D1 part2

--- day1_expense_report.py
+++ day1_expense_report.py
@@ -1,7 +1,19 @@
-# Approach: Starting from top of list, subtract value from 2020
-# Search remainder of list for result.
-# If found, get product and finish, if not, go to next entry in list.
+# Find the two values from puzzle_one_input file that totals 2020
+# Return the product
 
-# read input file into tuple
-# loop through list using logic above
+TOTAL = 2020
+current_values = []
+test_value = ''
+
+with open('puzzle_one_input', 'r') as f:
+    current_values = f.read().split('\n')
+
+for value in current_values:
+    test_value = TOTAL - int(value)
+
+    if str(test_value) in current_values:
+        break
+
+print(f"{value} + {test_value} = {int(value) + int(test_value)}.")
+print(f"The product of the two numbers is {test_value * int(value)}")
 
