2c27637bfd6d9a94d5c822fa164cc3169dff657b
Kelly Netterville <bitgrease@gmail.com>
2020-12-02 23:26:36 -0500

    Solution code for Day1 part 2

--- day1_expense_report_2.py
+++ day1_expense_report_2.py
@@ -1,25 +1,17 @@
 # Find the three values from puzzle_one_input file that totals 2020
-# Return the product
-
-# Subtract first value from 2020 result stored as test value
-# Subtract first value idx +1 from test value - remainder is new test value
-# see if new test is in array.
-# if not, repeat with next entry in the list.
-
-# Alternate
-# First + 2 + 3
-# First + 2 + 4
-# First + 3 + 4
-
 
 TOTAL = 2020
 expenses = []
-value1 = ''
-value2 = ''
-value3 = ''
 
 with open('puzzle_one_input', 'r') as f:
-    current_values = [int(value) for value in f.read().split('\n')]
+    expenses = [int(value) for value in f.read().split('\n')]
 
-
-print(current_values)
+for idx1 in range(len(expenses) - 2):
+    for idx2 in range(1, len(expenses) - 1):
+        for idx3 in range(2, len(expenses)):
+            total = expenses[idx1] + expenses[idx2] + expenses[idx3]
+            if total == TOTAL:
+                print(f"value1 is {expenses[idx1]}")
+                print(f"value2 is {expenses[idx2]}")
+                print(f"value3 is {expenses[idx3]}")
+                exit()
